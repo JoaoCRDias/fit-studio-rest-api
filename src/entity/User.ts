6f359c364e206581f23458c5af96b43f57786a39
JoaoCRDias <joaocrdias17@gmail.com>
2022-05-30 21:00:57 -0300

    chore(orm): setup typeorm

---
+++ src/entity/User.ts
@@ -0,0 +1,14 @@
+import { Entity, Column } from 'typeorm';
+import BaseEntity from './BaseEntity';
+
+@Entity()
+export class User extends BaseEntity {
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  age: number;
+}
--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,22 @@ import 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import { AppDataSource } from './data-source';
+
 const app = express();
 
 app.set('port', process.env.APP_PORT);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(app.get('port'), () => {
-  console.log(`   Server listening on port ${process.env.APP_PORT}\n`);
-  console.log('   Press CTRL-C to stop\n');
+app.listen(app.get('port'), async () => {
+  try {
+    await AppDataSource.initialize();
+    console.log(`   Server listening on port ${process.env.APP_PORT}\n`);
+    console.log('   Press CTRL-C to stop\n');
+  } catch (e) {
+    console.error('Error: ', e);
+  }
 });
 
 export default app;
